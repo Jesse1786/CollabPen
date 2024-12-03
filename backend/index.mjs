df7f549466c0909b2849d906f924ab06b3903282
Jun Kai Zhang <junkaizhang8@gmail.com>
2024-12-02 19:09:47 -0500

    Update cookie flags

--- backend/index.mjs
+++ backend/index.mjs
@@ -42,7 +42,7 @@ app.use(
     cookie: {
       secure: process.env.ENV === "prod",
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
